7fe318720f393dc4a40d7946162841f478473f57
Jordane Giuly <jordane@spendesk.com>
2016-06-30 19:55:38 +0200

    update pre-deploy script

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # TODO: argument patch
 
-npm version patch -m "release v%s"
-git push
+RELEASE="$1"
+if [ $RELEASE != "major" ] && [ $RELEASE != "minor" ] && [ $RELEASE != "patch" ];
+then
+	echo "You should provide with a new tag argument: major, minor or patch."
+	echo "Provided argument: $RELEASE"
+	exit 1
+fi
+
+npm version $RELEASE -m "release v%s"
+# git push
+
+exit 0
