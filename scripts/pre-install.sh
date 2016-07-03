89606bd0918e76fa0f5cb2ed04529ed5f22007cb
Jordane Giuly <jordane@spendesk.com>
2016-07-03 13:31:53 +0200

    simplify script

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum update
-sudo yum install gcc-c++ make
-sudo yum install openssl-devel
-sudo yum install git
-git clone git://github.com/nodejs/node.git
+# sudo yum update
+# sudo yum install gcc-c++ make
+# sudo yum install openssl-devel
+# sudo yum install git
+# git clone git://github.com/nodejs/node.git
